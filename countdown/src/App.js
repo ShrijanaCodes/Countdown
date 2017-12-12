96983560bae7787aa7e221c5359eb24ac0a4e9bb
Shrijana Ghimire <shrija2016@gmail.com>
2017-12-12 11:20:09 -0500

    add index and css file

---
+++ countdown/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component{
+	render(){
+	return(
+	<div> 
+		<div className = "App"> Countdown to January 1, 2018 
+		<div>
+		   <div>15 Days</div>
+		   <div>30 Hours</div>
+		   <div>15 Minutes</div>
+		   <div>20 Seconds</div>
+
+		</div>
+		<div>
+		<input placeholder = 'new date'/>
+		<button>Submit</button>
+		</div>
+	 </div>
+	 </div>
+	)
+	}
+}
+
+export default App;
