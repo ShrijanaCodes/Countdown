eae3e04c0bfa97c3128861f2d4bcc151cfa38475
ShrijanaCodes <shrija2016@gmail.com>
2017-12-13 17:36:13 -0500

    Update App.js

--- countdown/src/App.js
+++ countdown/src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-
 class App extends Component{
+	constructor(props){
+			super(props);
+			this.state = {
+				deadline: 'December 25, 2017',
+				newDeadline: ''
+			}
+		}
+
+		changeDeadline(){
+			this.setState({deadline: this.state.newDeadline});
+		}
 	render(){
+		
 	return(
 	<div> 
 		<div className = "App"> 
-		<div className = "App Title">Countdown to January 1, 2018 </div>
+		<div className = "App Title">
+		Countdown to {this.state.deadline} </div>
 		<div>
 		   <div>15 Days</div>
 		   <div>30 Hours</div>
@@ -15,8 +27,9 @@ class App extends Component{
 
 		</div>
 		<div>
-		<input placeholder = 'new date'/>
-		<button>Submit</button>
+		<input placeholder = 'new date'
+		onChange ={event => this.setState({newDeadline: event.target.value})}/>
+		<button onClick={() => this.changeDeadline()}>Submit</button>
 		</div>
 	 </div>
 	 </div>
